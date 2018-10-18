d628cd434d87c4cfc7ffd1da565cff1e61f7b349
Mateusz Szczęsny <mateusz.szczesny@contractors.roche.com>
2018-10-18 17:17:00 +0200

    add borrow model

--- app/src/main/java/pl/lodz/p/whoborrowedthat/controller/BorrowedListActivity.java
+++ app/src/main/java/pl/lodz/p/whoborrowedthat/controller/BorrowedListActivity.java
@@ -3,6 +3,8 @@ package pl.lodz.p.whoborrowedthat.controller;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import pl.lodz.p.whoborrowedthat.R;
+
 public class BorrowedListActivity extends AppCompatActivity {
 
     @Override
