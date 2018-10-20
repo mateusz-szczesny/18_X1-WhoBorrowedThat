1a6198067c2cc7717db6be3c332e03e84dc48b86
szczesnymateo <matty.szczesny@gmail.com>
2018-10-20 16:08:13 +0200

    add sp for logged user

---
+++ app/src/main/java/pl/lodz/p/whoborrowedthat/helper/ConstHelper.java
@@ -0,0 +1,6 @@
+package pl.lodz.p.whoborrowedthat.helper;
+
+public class ConstHelper {
+    public static final String USER_DATA__SP = "USER_DATA";
+    public static final String USER_LOGIN_STATUS__SP = "USER_LOGIN_STATUS";
+}
